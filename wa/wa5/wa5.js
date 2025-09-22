d27134098fe82df461290150218c25d96cc153d8
Connor Zeitlin <coze2389@colorado.edu>
2025-09-21 21:18:26 -0600

    wa5Nav

---
+++ wa/wa5/wa5.js
@@ -0,0 +1,7 @@
+  const navToggle =document.querySelector('.nav-toggle');
+    const navMenu =document.querySelector('.nav-menu2');
+const burger = document.getElementById('burger');
+
+burger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
