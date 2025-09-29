d8ff5158a67ea75a596165d79885ce5adbb0a8a8
Connor Zeitlin <coze2389@colorado.edu>
2025-09-28 22:09:54 -0600

    Rugby page base

--- wa/wa6/wa6.js
+++ wa/wa6/wa6.js
@@ -1,11 +1,24 @@
-  const navToggle =document.querySelector('.nav-toggle');
-    const navMenu =document.querySelector('.nav-menu2');
+const navToggle =document.querySelector('.nav-toggle');
+const navMenu =document.querySelector('.nav-menu2');
 const burger = document.getElementById('burger');
+const deez = document.getElementById('deez');
+const nuts = document.getElementById('nuts');
+const slime = document.getElementById('slime');
+
 
 burger.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
 
 function cFunction(){
-  alert("I am a Button");
+  alert("Follow our instagram for more timley updates");
 }
+//for the three size changing image boxes that have no images yet
+const tripbs = document.querySelectorAll('.tripb');
+
+  tripbs.forEach(tripb => {
+    tripb.addEventListener('click', () => {
+      tripbs.forEach(t => t.classList.remove('expanded'));
+      tripb.classList.add('expanded');
+    });
+  });
